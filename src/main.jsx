dcac869c988c641e5c195ebad63eb547a2b6784e
matihullahIT <matihurrehman.official@gmail.com>
2024-10-08 12:28:08 +0500

    Refactor main.jsx to include ToastContainer and CookiesProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {UserProvider} from '../src/Contaxt/contaxt.jsx'
+import { ToastContainer } from 'react-toastify'
+import { CookiesProvider } from 'react-cookie';
+
 createRoot(document.getElementById('root')).render(
+
+    <>
+<CookiesProvider defaultSetOptions={{ path: '/' }}>
 <UserProvider>
     <App />
 </UserProvider>
+<ToastContainer autoClose={2000} />
+</CookiesProvider>
+    </>
 )
