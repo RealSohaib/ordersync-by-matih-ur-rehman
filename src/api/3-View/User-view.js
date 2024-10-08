1fa5aa7052f3a30ea75c2ef01c601b391bff732d
matihullahIT <matihurrehman.official@gmail.com>
2024-10-08 12:28:48 +0500

    Refactor User-view.js to include DisplayUser function and update CreateUser and ChangeCredentials functions

--- src/api/3-View/User-view.js
+++ src/api/3-View/User-view.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Loginuser,     ChangeCredentials,
+const { Loginuser, DisplayUser,    ChangeCredentials,
     DeleteUser,CreateUser } = require('../2-Controler/user-controler');
 const cors=require("cors")
 module.exports = function UserView(app) {
@@ -7,6 +7,11 @@ module.exports = function UserView(app) {
     app.use(express.json()); // Use express.json() to parse incoming JSON
 
     app.use(cors());
+
+    app.get("/user",(req,res)=>{
+        // res.send("endpoint is ok")
+        DisplayUser(req,res)
+    })
     // Route for login
     app.post("/user/adduser", (req, res) => {
         CreateUser(req, res);
