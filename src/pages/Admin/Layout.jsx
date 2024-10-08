7a1ef8cc6b87fcfa3b4b5e876214d0e5a4179113
matihullahIT <matihurrehman.official@gmail.com>
2024-10-08 12:29:50 +0500

    Refactor Admin Layout component to include full screen width and capitalize text

--- src/pages/Admin/Layout.jsx
+++ src/pages/Admin/Layout.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import {AdminNavbar} from "../../constants"
 const Layout = ({children}) => {
   return (
-    <div className='flex'>
+    <div className='flex w-screen capitalize'>
       <Navbar>
         {AdminNavbar.map((item, index) => (
         <div
@@ -11,12 +11,12 @@ const Layout = ({children}) => {
         className="text-white m-2 hover:bg-gray-900 p-7 font-bold hover:border-l-4 transition-all hover:translate-x-2 flex"
       >
         <Link to={item.link} className="">
-          {item.label}
+          {item.icon}{item.label}
         </Link>
       </div>
         ))}
       </Navbar>
-      <div className='  border-3 border-black w-full flex flex-col items-center '>
+      <div className='  border-3 border-black w-screen flex flex-col items-center '>
       {children}
       </div>
     </div>
