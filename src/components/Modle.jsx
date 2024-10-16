9913101fd16c0643bd21ba68f033e2edfe013089
matihullahIT <matihurrehman.official@gmail.com>
2024-10-16 11:47:36 +0500

    Refactor Modal component to improve code readability and update prop names

--- src/components/Modle.jsx
+++ src/components/Modle.jsx
@@ -1,27 +1,30 @@
+import PropTypes from 'prop-types';
+import{X} from 'lucide-react';
+const Modal = ({ isOpen, onClose, title, children, closable = true }) => {
+  if (!isOpen) return null;
 
-const Modal = ({ isOpen, onClose, title, children, btnTitle,onClick,type  }) => {
-    if (!isOpen) return null;
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-          <h2 className="text-2xl font-bold mb-4">{title}</h2>
-          {children}
-          <div className=" flex justify-between w-full">
-          <button
-            onClick={onClose}
-            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
-            >
-            Close
-          </button>
-            <button 
-            type={type}
-            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
-            onClick={onClick}>
-              {btnTitle}
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
+      <div className="bg-white rounded-lg shadow-xl max-w-md w-full">
+        <div className="flex justify-between items-center p-4 border-b">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          {closable && (
+            <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+              <X size={24} />
             </button>
-            </div>
+          )}
         </div>
+        <div className="p-4">{children}</div>
       </div>
-    );
-  };
-  export default Modal
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  closable: PropTypes.bool,
+};
+export default  Modal;
