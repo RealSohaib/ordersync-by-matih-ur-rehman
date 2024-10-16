e9b9e640ba41614f2096975960f9e1379ba22dd1
matihullahIT <matihurrehman.official@gmail.com>
2024-10-16 11:46:25 +0500

    Refactor OrderDetails-view.js to add route for placing an order

--- src/api/3-View/OrderDetails-view.js
+++ src/api/3-View/OrderDetails-view.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { DisplayOrderDetails } = require('../2-Controler/OrderDetails-controler');
-
+const { DisplayOrderDetails, PlaceOrder } = require('../2-Controler/OrderDetails-controler');
 
 module.exports = function MenuView(app) {
-    // Set up Multer storage configuration
-    // Middleware to parse JSON
     app.use(express.json()); // Use express.json() to parse incoming JSON
-    
+
     // Route for displaying order details
-    app.get("/orderdetails", (req, res) => {
+    app.get("/orders", (req, res) => {
         DisplayOrderDetails(req, res);
     });
 
+    // Route for placing an order
+    app.post("/orders", (req, res) => {
+        PlaceOrder(req, res);
+    });
 };
