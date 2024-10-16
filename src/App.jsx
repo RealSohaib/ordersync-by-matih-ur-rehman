753dbc26fc2279f83a199ede9165064e13c56e00
matihullahIT <matihurrehman.official@gmail.com>
2024-10-16 11:44:46 +0500

    Refactor App.jsx to improve code readability and fix conditional rendering of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Client from './pages/Client/index';
-import AdminHome from './pages/Admin/home'
-import EmployeeHome from './pages/Employees/home'
+import AdminHome from './pages/Admin/home';
+import EmployeeHome from './pages/Employees/home';
 import Resciept from './pages/Client/Resciept';
-import Login from './components/Login'
+import Login from './components/Login';
 import Manageemployees from './pages/Admin/ManageEmployees';
 import ManageProfile from './pages/Admin/ManageProfile';
+import Error from './components/404';
+import { useCookies } from 'react-cookie';
+
 const App = () => {
-  // const [user,setUser]=useUser()
+  const [cookies] = useCookies(['user', 'client']);
+  const user = cookies.user;
+
   return (
     <div>
       <Router>
         <Routes>
-
           <Route path="/" element={<Client />} />
           <Route path="/resciept" element={<Resciept />} />
-          <Route path="/login" element={<Login />} />
-          {/*  in follwing conditions we have applied conditional rendering if the user role is admin then
-          enable admin pages but if the user is employee then enable employee pages
-          */}
-
-    <Route path="/admin" element={<AdminHome />} />
-    <Route path="/manageemployees" element={<Manageemployees />} />
-    <Route path="/profile" element={<ManageProfile />} />
-
-    <Route path="/employee" element={<EmployeeHome />} />
+          {user && user.role === 'admin' ? (
+            <>
+              <Route path="/admin" element={<AdminHome />} />
+              <Route path="/manageemployees" element={<Manageemployees />} />
+              <Route path="/profile" element={<ManageProfile />} />
+            </>
+          ) : (
+            <Route path="/login" element={<Login />} />
+          )}
+          {user && user.role === 'employee' ? (
+            <Route path="/employee" element={<EmployeeHome />} />
+          ) : (
+            <Route path="/login" element={<Login />} />
+          )}
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
