6d3156e8d3d35d45838128167f33246296023957
matihullahIT <matihurrehman.official@gmail.com>
2024-11-03 15:52:19 +0500

    Refactor Navbar component to implement admin navigation links and improve toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa"; // Import icons for the toggle button
+import { Link } from "react-router-dom";
+import { AdminNavbar } from "../constants"; // Import AdminNavbar
 
-const Navbar = ({ children }) => {
+const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const ToggleBtn = () => {
@@ -10,22 +12,34 @@ const Navbar = ({ children }) => {
 
   return (
     <>
-      <div className="relative top-0 left-0 z-50 flex items-center p-4 bg-black text-white w-full md:hidden">
+      <div className="top-0 left-0 z-50 flex items-center p-4 bg-black text-white w-full md:hidden">
         <button onClick={ToggleBtn} className="relative text-white text-2xl">
           {toggle ? <FaTimes /> : <FaBars />}
         </button>
       </div>
       <div
-        className={`fixed top-0 left-0 h-screen bg-black text-white transition-transform transform ${
+        className={`stickey z-10 top-0 left-0 h-screen bg-black text-white transition-transform transform ${
           toggle ? "translate-x-0" : "-translate-x-full"
         } md:translate-x-0 md:relative md:w-64`}
       >
-        <div className=" md:hidden">
+        <div className="md:hidden p-4">
           <button onClick={ToggleBtn} className="relative text-white text-2xl">
             {toggle ? <FaTimes /> : <FaBars />}
           </button>
         </div>
-        <div className="p-4">{children}</div>
+        <div className="p-4">
+          {AdminNavbar.map((item, index) => (
+            <Link
+              key={index}
+              to={item.link}
+              className="flex items-center p-4 text-white hover:bg-gray-900 hover:border-l-4 transition-all"
+              onClick={() => setToggle(false)} // Close the menu on link click
+            >
+              <item.icon className="mr-3" />
+              {item.label}
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
