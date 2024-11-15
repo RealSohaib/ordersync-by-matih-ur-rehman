f5b7d8e3a2c58166a60bb7603a921aa5ae45f32c
matihullahIT <matihurrehman.official@gmail.com>
2024-11-15 23:35:21 +0500

    Add Finance and Inventory views to API

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const Userview = require("./3-View/User-view");
 const Menuview = require("./3-View/Menu-view");
 const OrderView = require("./3-View/OrderDetails-view");
+const FinanceView=require("./3-View/Finance-view")
+const Inventory=require("./3-View/inventor-view")
 const Connection =require('./connection');
 let port = 3001;
 
@@ -11,6 +13,9 @@ try{
     Userview(app);
     Menuview(app);
     OrderView(app);
+    FinanceView(app)
+    Inventory(app)
+
 }
 catch(err){
     console.log("your server is not woking properly",err)
