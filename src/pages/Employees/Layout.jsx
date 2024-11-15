0e4ea0a033cc0754dc2258f28b58e057811a279e
matihullahIT <matihurrehman.official@gmail.com>
2024-11-15 23:42:42 +0500

    Refactor Employees Layout: enhance mobile responsiveness and improve navigation structure

--- src/pages/Employees/Layout.jsx
+++ src/pages/Employees/Layout.jsx
@@ -1,38 +1,47 @@
-import Navbar from '../../components/Navbar';
-import { EmployeesNavbar } from '../../constants/index';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Navbar from '../../components/Navbar.jsx';
+import { EmployeesNavbar } from '../../constants/index.jsx';
+import {Link} from 'react-router-dom'
+import { useState } from 'react';
 
-const Layout= ({children}) => {
+const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isMobile = window.innerWidth <= 768;
+  useEffect(() => {
+    document.title = "Employee Panel";
+  }, []);
+
+  useEffect(() => {
+    console.log("navItems in Layout:"); // Debugging: Check if navItems are received correctly
+  }, []);
+
   return (
-    <>
-    <div className='flex '>
-    <Navbar>
-      <div>
-        <div className="text-white flex justify-between items-center p-4 md:hidden">
-          <h1 className="text-xl font-bold">Employees Panel</h1>
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-100">
+      <Navbar>
+      {EmployeesNavbar.map((item, index) => (
+            <Link
+              key={index}
+              to={item.link}
+              className="flex items-center p-3 mb-2 text-white hover:bg-gray-700 rounded-lg transition-all"
+              onClick={() => isMobile && setIsOpen(false)}
+            >
+              <item.icon className="mr-3 text-xl" />
+              <span>{item.label}</span>
+            </Link>
+          ))}
+      </Navbar>
+      <main className="flex-1 p-4 md:p-8 mt-16 md:mt-0 md:ml-64">
+        <div className="max-w-7xl mx-auto overflow-auto">
+          {children}
         </div>
-        <div className="flex flex-col">
-            {EmployeesNavbar.map((item, index) => (
-              <div
-                key={index}
-                className="text-white m-2 hover:bg-gray-900 p-7 font-bold hover:border-l-4 transition-all hover:translate-x-2 flex"
-              >
-                <Link to={item.link} className="ml-2">
-                  {item.label}
-                </Link>
-              </div>
-            ))}
-        </div>
-      </div>
-      <div>
-      </div>
-    </Navbar>
-    <div className='border-3 border-black w-full'>
-    {children}
+      </main>
     </div>
-    </div>
-    </>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Layout;
