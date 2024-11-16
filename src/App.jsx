02c6a6596d3e59e001a966455f453c69329328c4
matihullahIT <matihurrehman.official@gmail.com>
2024-11-16 15:10:43 +0500

    app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Client from './pages/Client/index';
 import AdminHome from './pages/Admin/home';
 import EmployeeHome from './pages/Employees/home';
+import MangeOrder from './pages/Employees/ManageOrders';
+import MangeInventoryemployee from './pages/Employees/ManageInventory';
+import EmpployeesAccounts from './pages/Employees/ManageAccounts';
 import Resciept from './pages/Client/Resciept';
 import Login from './components/Login';
 import Manageemployees from './pages/Admin/ManageEmployees';
 import ManageProfile from './pages/Admin/ManageProfile';
 import OrderDetials from './pages/Admin/ManageOrders.jsx';
+import Accounts from './pages/Admin/ManageAccounts.jsx';
 // import OrderDetials from './pages/Admin';
 import ManageInventory from './pages/Admin/ManageInventory.jsx';
 import Error from './components/404';
@@ -30,12 +34,19 @@ const App = () => {
               <Route path="/orderdetails" element={<OrderDetials />} />
               <Route path="/profile" element={<ManageProfile />} />
               <Route path="/manageinventory" element={<ManageInventory />} />
+              <Route path="/accounts" element={<Accounts />} />
             </>
           ) : (
             <Route path="/login" element={<Login />} />
           )}
           {employee  ? (
+            <>
             <Route path="/employee" element={<EmployeeHome />} />
+            <Route path="/orders" element={<MangeOrder />} />
+            <Route path="/accounts" element={<EmpployeesAccounts />} />
+            <Route path="/inventory" element={<MangeInventoryemployee />} />
+
+            </>
           ) : (
             <Route path="/login" element={<Login />} />
           )}
