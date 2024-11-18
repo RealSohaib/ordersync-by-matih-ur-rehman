c75d75ba8fb4c866ae9e0c5c38e2e31dacb036da
matihullahIT <matihurrehman.official@gmail.com>
2024-11-18 07:29:11 +0500

    Add ChangePassword route and refactor user-related routes in User-view

--- src/api/3-View/User-view.js
+++ src/api/3-View/User-view.js
@@ -1,31 +1,39 @@
-
 const express = require('express');
-const { Loginuser, DisplayUser,    ChangeCredentials,
-    DeleteUser,CreateUser } = require('../2-Controler/user-controler');
-const cors=require("cors")
+const { Loginuser, DisplayUser, ChangeCredentials, ChangePassword, DeleteUser, CreateUser } = require('../2-Controler/user-controler');
+const cors = require("cors");
+
 module.exports = function UserView(app) {
-    console.log("user view is working")
+    console.log("user view is working");
     // Middleware to parse JSON
     app.use(express.json()); // Use express.json() to parse incoming JSON
 
     app.use(cors());
 
-    app.get("/user",(req,res)=>{
-        // res.send("endpoint is ok")
-        DisplayUser(req,res)
-    })
-    // Route for login
+    app.get("/user", (req, res) => {
+        DisplayUser(req, res);
+    });
+
+    // Route for creating a new user
     app.post("/user/adduser", (req, res) => {
         CreateUser(req, res);
     });
+
+    // Route for login
     app.post("/user/login", (req, res) => {
         Loginuser(req, res);
     });
 
-    // Route for changing password
+    // Route for changing credentials
     app.put("/user/edit", (req, res) => {
         ChangeCredentials(req, res);
     });
+
+    // Route for changing password
+    app.put("/user/change-password", (req, res) => {
+        ChangePassword(req, res);
+    });
+
+    // Route for deleting a user
     app.delete("/user/remove", (req, res) => {
         DeleteUser(req, res);
     });
